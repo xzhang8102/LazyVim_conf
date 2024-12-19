7e3110f507fefdf7c87b43a143ceccf1a31ee56f
zxw <xzhang305@gmail.com>
2024-12-19 16:35:42 +0800

    update customized configs

--- lua/plugins/customs.lua
+++ lua/plugins/customs.lua
@@ -7,21 +7,21 @@ return {
     opts = {},
   },
   { "folke/flash.nvim", enabled = false },
-  {
-    "hrsh7th/nvim-cmp",
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-      local cmp = require("cmp")
-      opts.mapping["<ESC>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.abort() -- restore the original input
-        else
-          fallback()
-        end
-      end, { "i" })
-      opts.experimental.ghost_text = false
-    end,
-  },
+  -- {
+  --   "hrsh7th/nvim-cmp",
+  --   ---@param opts cmp.ConfigSchema
+  --   opts = function(_, opts)
+  --     local cmp = require("cmp")
+  --     opts.mapping["<ESC>"] = cmp.mapping(function(fallback)
+  --       if cmp.visible() then
+  --         cmp.abort() -- restore the original input
+  --       else
+  --         fallback()
+  --       end
+  --     end, { "i" })
+  --     opts.experimental.ghost_text = false
+  --   end,
+  -- },
   {
     "nvim-telescope/telescope.nvim",
     opts = function(_, opts)
@@ -51,15 +51,19 @@ return {
   },
   {
     "neovim/nvim-lspconfig",
-    opts = {
+    opts = function(_, opts)
       ---@type vim.diagnostic.Opts
-      diagnostics = {
+      opts.diagnostics = {
         float = {
           border = "rounded",
         },
         virtual_text = false,
-      },
-    },
+      }
+
+      opts.servers.gopls = {
+        mason = not vim.fn.executable("gopls"),
+      }
+    end,
   },
   {
     "nvim-neo-tree/neo-tree.nvim",
@@ -79,6 +83,18 @@ return {
     ---@param opts conform.setupOpts
     opts = function(_, opts)
       opts.formatters_by_ft["python"] = { "black" }
+    end,
+  },
+  {
+    "saghen/blink.cmp",
+    opts = function(_, opts)
+      opts.completion.ghost_text = {
+        enabled = false,
+      }
+      opts.keymap = {
+        preset = "default",
+        ["<ESC>"] = { "hide", "fallback" },
+      }
     end,
   },
 }
