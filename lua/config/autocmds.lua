fb0b41e5c510b61d6a7b285c21d466d470ad455a
zxw <xzhang305@gmail.com>
2025-03-04 13:32:10 +0800

    add mysql client integration

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,32 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+local group = vim.api.nvim_create_augroup("zxw_augroup", { clear = true })
+
+local function setup_mysql_client_integration()
+  local file_name = vim.fn.expand("%:t")
+
+  -- Check if the filename starts with 'sql' (MySQL client often uses this prefix)
+  local has_sql_prefix = file_name:match("^sql")
+  if not has_sql_prefix then
+    return
+  end
+
+  -- Additional check: see if process that spawned nvim was mysql
+  local parent_pid = vim.fn.system("ps -o ppid= -p " .. vim.fn.getpid()):match("%d+")
+  local grandparent_pid = vim.fn.system("ps -o ppid= -p " .. parent_pid):match("%d+")
+  local is_called_by_mysql =
+    vim.fn.system(string.format("ps -o comm= -p %s,%s", parent_pid, grandparent_pid)):match("mysql")
+
+  -- If conditions match, set up MySQL editing environment
+  if is_called_by_mysql then
+    vim.bo.filetype = "sql"
+  end
+end
+
+-- Run the function on every buffer read
+vim.api.nvim_create_autocmd({ "BufRead" }, {
+  pattern = "*",
+  group = group,
+  callback = setup_mysql_client_integration,
+})
