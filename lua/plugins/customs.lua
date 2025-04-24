6f8c1d4b684554e36a83c3309cb5fe7b13504861
zxw <xzhang305@gmail.com>
2025-04-24 13:42:14 +0800

    move command line to bottom

--- lua/plugins/customs.lua
+++ lua/plugins/customs.lua
@@ -182,8 +182,12 @@ return {
           enabled = false,
         },
       },
+      cmdline = {
+        view = "cmdline", -- moves command line to bottom
+      },
       presets = {
         lsp_doc_border = true,
+        command_palette = false, -- tab completions for commandline don't pop-up at top
       },
     },
   },
