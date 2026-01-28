c1abfa77db844130532da1000c4ff421de6a8ad5
zxw <xzhang305@gmail.com>
2026-01-28 16:53:24 +0800

    use clip.exe in WSL

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,4 +2,20 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.snacks_animate = false
-vim.g.clipboard = "osc52"
+
+-- ref :h clipboard-wsl
+if vim.fn.executable("clip.exe") == 1 then
+  vim.opt.clipboard = ""
+  vim.g.clipboard = {
+    name = "WSL_clipboard",
+    copy = {
+      ["+"] = "clip.exe",
+      ["*"] = "clip.exe",
+    },
+    paste = {
+      ["+"] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      ["*"] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+    cache_enabled = 0,
+  }
+end
